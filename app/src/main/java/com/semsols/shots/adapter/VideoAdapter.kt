fdb6f158d995e436d074d8ada53a5b1c8e829238
Aman Semwal <amansemwal542@gmail.com>
2022-02-19 12:49:44 +0900

    Shots

---
+++ app/src/main/java/com/semsols/shots/adapter/VideoAdapter.kt
@@ -0,0 +1,17 @@
+package com.semsols.shots.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.semsols.shots.model.VideoModel
+
+
+class VideoAdapter(options: FirebaseRecyclerOptions<VideoModel>):
+
+{
+    inner class MyViewHolder(v: View): RecyclerView.ViewHolder(v){
+
+
+    }
+
+}
---
+++ app/src/main/java/com/semsols/shots/model/VideoModel.kt
@@ -0,0 +1,8 @@
+package com.semsols.shots.model
+
+data class VideoModel(
+
+    val title:String? = null,
+    val desc:String? = null,
+    val url:String? = null
+)
